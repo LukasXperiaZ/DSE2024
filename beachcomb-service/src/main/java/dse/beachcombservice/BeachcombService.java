2d09c672176ebd22d0be7a25a01a4244ac61f19d
Oliver Mayer <e12023147@student.tuwien.ac.at>
2024-05-12 12:00:51 +0200

    beachcomb: added follow_me route with dummy impl
    Currently fetches all vehicles and puts 1,2,3 as list

--- beachcomb-service/src/main/java/dse/beachcombservice/BeachcombService.java
+++ beachcomb-service/src/main/java/dse/beachcombservice/BeachcombService.java
@@ -8,6 +8,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import javax.ejb.Singleton;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @Singleton
 @Component
@@ -21,5 +24,16 @@ public class BeachcombService {
     public void insert(VehicleDTO vehicleDTO) {
         IVehicleModel vehicleModel = modelMapper.map(vehicleDTO, LeadingVehicleModel.class);
         vehicleRepository.insert(vehicleModel);
+    }
+
+    public Map<String, List<String>> getFollowMeCandidates() {
+        //TODO: Add correct implementation
+        var allVehicles = vehicleRepository.findAll();
+        Map<String, List<String>> followMeCandidates = new HashMap<>();
+        for (IVehicleModel vehicle : allVehicles) {
+            LeadingVehicleModel vehicleModel = (LeadingVehicleModel) vehicle;
+            followMeCandidates.put(vehicleModel.getVin(), List.of("1", "2", "3"));
+        }
+        return followMeCandidates;
     }
 }
--- beachcomb-service/src/main/java/dse/beachcombservice/BeachcombServiceResource.java
+++ beachcomb-service/src/main/java/dse/beachcombservice/BeachcombServiceResource.java
@@ -1,14 +1,14 @@
 package dse.beachcombservice;
 
-import dse.beachcombservice.mongodb.models.IVehicleModel;
-import dse.beachcombservice.mongodb.models.LeadingVehicleModel;
-import dse.beachcombservice.mongodb.models.VehicleModel;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
+import java.util.List;
+import java.util.Map;
 
 @RestController
 public class BeachcombServiceResource {
@@ -18,9 +18,15 @@ public class BeachcombServiceResource {
 
     @PostMapping("/insert")
     public String insert(@Valid @RequestBody VehicleDTO vehicleModel) {
-        System.out.println(vehicleModel.getClass());
+        System.out.println("Inserting");
         beachcombService.insert(vehicleModel);
         return "Inserted!";
+    }
+
+    @GetMapping("/follow_me")
+    public Map<String, List<String>> followMe() {
+        System.out.println("Follow me");
+        return beachcombService.getFollowMeCandidates();
     }
 
 }
