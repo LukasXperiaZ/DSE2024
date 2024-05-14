33cd59d85fa7a8499c8340bdf61aec481fb5a03a
Oliver Mayer <e12023147@student.tuwien.ac.at>
2024-05-14 21:26:15 +0200

    beachcomb: Add service method that retrieves all followMeCandidates

--- beachcomb-service/src/main/java/dse/beachcombservice/BeachcombService.java
+++ beachcomb-service/src/main/java/dse/beachcombservice/BeachcombService.java
@@ -5,6 +5,7 @@ import dse.beachcombservice.mongodb.models.IVehicleModel;
 import dse.beachcombservice.mongodb.models.LeadingVehicleModel;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.geo.Point;
 import org.springframework.stereotype.Component;
 
 import javax.ejb.Singleton;
@@ -16,23 +17,24 @@ import java.util.Map;
 @Component
 public class BeachcombService {
 
-    private ModelMapper modelMapper = new ModelMapper();
+    private final ModelMapper modelMapper = new ModelMapper();
 
     @Autowired
     private VehicleRepository vehicleRepository;
 
     public void insert(VehicleDTO vehicleDTO) {
-        IVehicleModel vehicleModel = modelMapper.map(vehicleDTO, LeadingVehicleModel.class);
+        LeadingVehicleModel vehicleModel = modelMapper.map(vehicleDTO, LeadingVehicleModel.class);
         vehicleRepository.insert(vehicleModel);
     }
 
     public Map<String, List<String>> getFollowMeCandidates() {
-        //TODO: Add correct implementation
-        var allVehicles = vehicleRepository.findAll();
+        //TODO: Maybe change this if we consider storing all data and not only the newest location of a vehicle
+        var allVehicles = vehicleRepository.findByVinIsNotNull();
         Map<String, List<String>> followMeCandidates = new HashMap<>();
         for (IVehicleModel vehicle : allVehicles) {
-            LeadingVehicleModel vehicleModel = (LeadingVehicleModel) vehicle;
-            followMeCandidates.put(vehicleModel.getVin(), List.of("1", "2", "3"));
+            Point location = new Point(vehicle.getLocation().get(0), vehicle.getLocation().get(1));
+            List<LeadingVehicleModel> byLocationNear = vehicleRepository.findByLocationNear(vehicle.getVin(), location.getX(), location.getY(), 0.2);
+            followMeCandidates.put(vehicle.getVin(), byLocationNear.stream().map(IVehicleModel::getVin).toList());
         }
         return followMeCandidates;
     }
--- beachcomb-service/src/main/java/dse/beachcombservice/mongodb/VehicleRepository.java
+++ beachcomb-service/src/main/java/dse/beachcombservice/mongodb/VehicleRepository.java
@@ -1,9 +1,19 @@
 package dse.beachcombservice.mongodb;
 
 import dse.beachcombservice.mongodb.models.IVehicleModel;
+import dse.beachcombservice.mongodb.models.LeadingVehicleModel;
+import dse.beachcombservice.mongodb.models.VehicleLocation;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface VehicleRepository extends MongoRepository<IVehicleModel, String> {
+
+    @Query("{'vin': {$ne: ?0}, 'location': {$near: {$geometry: {type: 'Point', coordinates: [?1, ?2]}, $maxDistance: ?3}}}")
+    List<LeadingVehicleModel> findByLocationNear(String vin, double longitude, double latitude, double maxDistance);
+
+    List<VehicleLocation> findByVinIsNotNull();
 }
