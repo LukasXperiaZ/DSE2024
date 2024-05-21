9ee7fe4acf8bff35a62c4c377936750a3b0e5d8c
Lukas <e11911069@student.tuwien.ac.at>
2024-05-21 14:52:17 +0200

    Allowed cross origin requests for production.

--- python-services/python_services/inventory_service/main.py
+++ python-services/python_services/inventory_service/main.py
@@ -2,6 +2,7 @@ import logging
 
 import uvicorn
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from python_services.common.log_config import LOGGING_CONFIG
 from python_services.inventory_service import api
@@ -13,6 +14,13 @@ logging.config.dictConfig(LOGGING_CONFIG)
 
 app = FastAPI()
 app.include_router(api.router)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 if __name__ == "__main__":
