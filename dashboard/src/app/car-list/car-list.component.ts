71e7e3fd96843f619b160bd6586b580609dcd1e8
Lukas <e11911069@student.tuwien.ac.at>
2024-05-22 18:45:22 +0200

    Dashboard now is able to show the current position of the two vehicles.

--- dashboard/src/app/car-list/car-list.component.ts
+++ dashboard/src/app/car-list/car-list.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {InventoryService} from "../inventory.service";
 import {Car} from "../../dto/Car";
 import {MatList, MatListItem, MatListItemLine, MatListOption, MatSelectionList} from "@angular/material/list";
@@ -35,32 +35,21 @@ import {BeachCombService} from "../beach-comb.service";
   templateUrl: './car-list.component.html',
   styleUrl: './car-list.component.css'
 })
-export class CarListComponent {
+export class CarListComponent implements OnInit {
 
-  cars: Car[] | undefined
+  cars: Car[] | undefined;
 
-  vehicleData: VehicleData[] | undefined
+  enable = false;
 
-  constructor(private inventoryService: InventoryService,
-              private beachCombService: BeachCombService) {
-    this.getCars()
+  constructor(private inventoryService: InventoryService) {
+  }
+
+  ngOnInit() {
+    this.getCars();
   }
 
   getCars() {
     this.inventoryService.getCars()
       .subscribe(cars => this.cars = cars)
-  }
-
-  getVehicleDataTest() : VehicleData[] {
-    let vehicle = new VehicleData("XP7VGCEJXPB204655", new Coordinates(48.198141, Constants.FIRST_LANE_LAT), 110.0, 1, new Date());
-    let autonomousVehicle = new VehicleData("5YJ3E7EB7KF240393", new Coordinates(48.199064, Constants.FIRST_LANE_LAT), 100.0, 1, new Date());
-
-    let vehicleSecondLane = new VehicleData("SL", new Coordinates(48.198555, Constants.SECOND_LANE_LAT), 125.0, 2, new Date());
-    let vehicleThirdLane = new VehicleData("TL", new Coordinates(48.198777, Constants.THIRD_LANE_LAT), 130.0, 3, new Date());
-
-    let vehicleTopLeft = new VehicleData("TL", new Coordinates(48.199064, Constants.THIRD_LANE_LAT - Constants.LANE_DIFFERENCE/2.0), 130.0, 3, new Date());
-    let vehicleBottomRight = new VehicleData("BR", new Coordinates(48.198141, Constants.FIRST_LANE_LAT + Constants.LANE_DIFFERENCE/2.0), 130.0, 3, new Date());
-
-    return [vehicle, autonomousVehicle, vehicleSecondLane, vehicleThirdLane, vehicleTopLeft, vehicleBottomRight]
   }
 }
--- dashboard/src/app/car-svg/car-svg.component.ts
+++ dashboard/src/app/car-svg/car-svg.component.ts
@@ -1,38 +1,43 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {Component, Input, OnDestroy, OnInit} from '@angular/core';
 import {VehicleData} from "../../dto/VehicleData";
 import {Constants} from "../../constants/Constants";
+import {NgIf} from "@angular/common";
+import {Coordinates} from "../../dto/Coordinates";
 import {Car} from "../../dto/Car";
 import {BeachCombService} from "../beach-comb.service";
 
 @Component({
   selector: 'app-car-svg',
   standalone: true,
-  imports: [],
+  imports: [
+    NgIf
+  ],
   templateUrl: './car-svg.component.svg',
   styleUrl: './car-svg.component.css'
 })
-export class CarSvgComponent implements OnInit {
-  @Input() cars!: Car[];
+export class CarSvgComponent implements OnInit, OnDestroy {
+  @Input() carList!: Car[];
   vehicleData: VehicleData[] = [];
+
+  intervalId: number = -1;
 
   road = 'rgb(211, 211, 211)';
   white = 'rgb(255, 255, 255)';
   car = 'rgb(0, 0, 0)';
+  dist = 'rgb(15,46,169)';
 
   constructor(private beachCombService: BeachCombService) {
   }
 
   ngOnInit() {
-    this.updateVehicleData();
+    this.getVehicleData();
+
+    //setInterval(this.getVehicleData, 5000)
+    setInterval(this.getVehicleData.bind(this), 1000);
   }
 
-  updateVehicleData() {
-    let vehicleData: VehicleData[] = [];
-    for (let i = 0; i < this.cars.length; i++) {
-      this.beachCombService.getVehicleData(this.cars[i].vin)
-        .subscribe(data => {vehicleData.push(data)});
-    }
-    this.vehicleData = vehicleData;
+  ngOnDestroy() {
+    clearInterval(this.intervalId)
   }
 
   latitudeToX(latitude: number) {
@@ -45,34 +50,99 @@ export class CarSvgComponent implements OnInit {
     let maxLongitude = this.getMaxLongitude();
     let minLongitude = this.getMinLongitude();
     // "720 -" since the y coordinate decreases as one goes up
-    return 720 - (longitude - minLongitude) / (maxLongitude - minLongitude) * 720;
+    if (!isNaN(720 - (longitude - minLongitude) / (maxLongitude - minLongitude) * 720)) {
+      return 720 - (longitude - minLongitude) / (maxLongitude - minLongitude) * 720;
+    }
+    return 0;
   }
 
   getMaxLongitude(): number {
-    if (this.vehicleData.length > 0) {
-      let maxLongitude = this.vehicleData[0].coordinates.longitude;
-      for (let i = 0; i < this.vehicleData.length; i++) {
-        let currLongitude = this.vehicleData[i].coordinates.longitude;
-        if (currLongitude > maxLongitude) {
-          maxLongitude = currLongitude;
-        }
+    let maxLongitude = -1;
+    for (let i = 0; i < this.vehicleData.length; i++) {
+      let currLongitude = this.vehicleData[i].coordinates.longitude;
+      if (currLongitude > maxLongitude) {
+        maxLongitude = currLongitude;
       }
-      return maxLongitude;
     }
-    return -1;
+    return maxLongitude;
   }
 
   getMinLongitude(): number {
-    if (this.vehicleData.length > 0) {
-      let minLongitude = this.vehicleData[0].coordinates.longitude;
-      for (let i = 0; i < this.vehicleData.length; i++) {
-        let currLongitude = this.vehicleData[i].coordinates.longitude;
-        if (currLongitude < minLongitude) {
-          minLongitude = currLongitude;
-        }
+    let minLongitude = 1000000;
+    for (let i = 0; i < this.vehicleData.length; i++) {
+      let currLongitude = this.vehicleData[i].coordinates.longitude;
+      if (currLongitude < minLongitude) {
+        minLongitude = currLongitude;
       }
-      return minLongitude;
     }
-    return -1;
+    return minLongitude;
   }
+
+  getVehicleData() {
+    let newVehicleData: VehicleData[] = [];
+    for (let i = 0; i < this.carList.length; i++) {
+      this.beachCombService.getVehicleData(this.carList[i].vin)
+        .subscribe(newVehicle => {
+            newVehicleData.push(newVehicle);
+        }
+        );
+    }
+    this.vehicleData = newVehicleData;
+  }
+
+  getDistanceLineCoordinates() {
+    let vehicle1 = this.vehicleData[0];
+    let vehicle2 = this.vehicleData[1];
+    if (vehicle1.vin == "XP7VGCEJXPB204655") {
+      let tempVehicle = vehicle2;
+      vehicle2 = vehicle1;
+      vehicle1 = tempVehicle;
+    }
+
+    let vehicle1X = this.latitudeToX(vehicle1.coordinates.latitude)
+    let vehicle1Y = this.longitudeToY(vehicle1.coordinates.longitude)
+
+    let vehicle2X = this.latitudeToX(vehicle2.coordinates.latitude)
+    let vehicle2Y = this.longitudeToY(vehicle2.coordinates.longitude)
+
+    let distance = this.getDistanceFromLatLonInM(vehicle1.coordinates.latitude, vehicle1.coordinates.longitude,
+      vehicle2.coordinates.latitude, vehicle2.coordinates.longitude);
+
+    return [vehicle1X, vehicle1Y, vehicle2X, vehicle2Y, distance]
+  }
+
+  // ----- From https://stackoverflow.com/questions/18883601/function-to-calculate-distance-between-two-coordinates -----
+  getDistanceFromLatLonInM(lat1: number, lon1: number, lat2: number, lon2: number) {
+    var R = 6371000; // Radius of the earth in m
+    var dLat = this.deg2rad(lat2-lat1);  // deg2rad below
+    var dLon = this.deg2rad(lon2-lon1);
+    var a =
+      Math.sin(dLat/2) * Math.sin(dLat/2) +
+      Math.cos(this.deg2rad(lat1)) * Math.cos(this.deg2rad(lat2)) *
+      Math.sin(dLon/2) * Math.sin(dLon/2)
+    ;
+    var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+    var d = R * c; // Distance in m
+    return d;
+  }
+
+  deg2rad(deg: number) {
+    return deg * (Math.PI/180)
+  }
+  // ----- end -----
+
+  getVehicleDataTest() {
+    let vehicle = new VehicleData("XP7VGCEJXPB204655", new Coordinates(48.198141, Constants.FIRST_LANE_LAT), 110.0, 1, new Date());
+    let autonomousVehicle = new VehicleData("5YJ3E7EB7KF240393", new Coordinates(48.199064, Constants.FIRST_LANE_LAT), 100.0, 1, new Date());
+
+    let vehicleSecondLane = new VehicleData("SL", new Coordinates(48.198555, Constants.SECOND_LANE_LAT), 125.0, 2, new Date());
+    let vehicleThirdLane = new VehicleData("TL", new Coordinates(48.198777, Constants.THIRD_LANE_LAT), 130.0, 3, new Date());
+
+    let vehicleTopLeft = new VehicleData("TL", new Coordinates(48.199064, Constants.THIRD_LANE_LAT - Constants.LANE_DIFFERENCE / 2.0), 130.0, 3, new Date());
+    let vehicleBottomRight = new VehicleData("BR", new Coordinates(48.198141, Constants.FIRST_LANE_LAT + Constants.LANE_DIFFERENCE / 2.0), 130.0, 3, new Date());
+
+    this.vehicleData = [vehicle, autonomousVehicle, vehicleSecondLane, vehicleThirdLane, vehicleTopLeft, vehicleBottomRight];
+  }
+
+  protected readonly Math = Math;
 }
