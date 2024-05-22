7469db4dd19b120077e273f55a726d5dbfef274d
Lukas <e11911069@student.tuwien.ac.at>
2024-05-22 16:30:09 +0200

    Removed unused imports

--- datafeeder/src/main/java/dse/datafeeder/rabbitMq/RabbitMq.java
+++ datafeeder/src/main/java/dse/datafeeder/rabbitMq/RabbitMq.java
@@ -7,9 +7,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
-import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
 @Service
